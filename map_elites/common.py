8bffdef17eceaf13c330b90c80ee1e65e1a5c103
Ioana Mihailescu <ioana.mihailescu20@imperial.ac.uk>
2024-05-09 16:21:45 +0100

    remove start method

--- map_elites/common.py
+++ map_elites/common.py
@@ -48,8 +48,6 @@ import random
 from collections import defaultdict
 from sklearn.cluster import KMeans
 
-if __name__ == '__main__':
-    torch.multiprocessing.set_start_method('spawn')
 default_params = \
     {
         # more of this -> higher-quality CVT
